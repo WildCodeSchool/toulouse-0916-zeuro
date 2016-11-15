896cc038b81d02aed75afbf6319aab58ca9ba5c4
apprenti <pchbenoit31@gmail.com>
2016-11-15 16:54:33 +0100

    rajout main activity

--- app/src/main/java/fr/wildcodeschool/zeuro/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/zeuro/MainActivity.java
@@ -1,13 +1,45 @@
 package fr.wildcodeschool.zeuro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private Button  recherche, profil;
+    private Switch notif;
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = (TextView) findViewById(R.id.slogan);
+        logo = (ImageView) findViewById(R.id.image_logo);
+        notif = (Switch) findViewById(R.id.switch1);
+        recherche = (Button) findViewById(R.id.rechercheButton);
+        profil = (Button) findViewById(R.id.profilButton);
+
+        recherche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rechercheIntent = new Intent(MainActivity.this, RechercheActivity.class);
+                startActivity(rechercheIntent);
+            }
+        });
+        profil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profilIntent = new Intent(MainActivity.this, ProfilActivity.class);
+                startActivity(profilIntent);
+            }
+        });
     }
 }
