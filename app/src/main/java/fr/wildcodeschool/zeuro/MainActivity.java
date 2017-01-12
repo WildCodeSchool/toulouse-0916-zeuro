1dee5c2c4aa1de16fd04902bb5c7c9c3f3443c41
clément mezerette <clement.mezerette@gmail.com>
2017-01-12 18:49:55 +0100

    refactor

--- app/src/main/java/fr/wildcodeschool/zeuro/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/zeuro/MainActivity.java
@@ -1,23 +1,15 @@
 package fr.wildcodeschool.zeuro;
 
-import android.app.Fragment;
 import android.content.Intent;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import fr.wildcodeschool.zeuro.fragment.ListFragment;
@@ -84,12 +76,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.forfaits_menu) {
             // Handle the camera action
 
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.assurances_menu) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.banque_menu) {
 
         }
 
